51ed167bff04723c1400088a293c1a51fa80d03c
miconer <roger.micone@autodesk.com>
2017-02-10 12:36:14 -0800

    no puts for tags.size

--- lib/inspec/aws/resources/taggable_base.rb
+++ lib/inspec/aws/resources/taggable_base.rb
@@ -3,7 +3,7 @@
 class TaggableBase < EC2Base
   def has_tag?(tag_key = nil, value = nil)
     tags = @resource.tags
-    puts tags.size
+    # puts tags.size
     return tags.size > 0 if tag_key.nil?
     tags.each do |tag|
       if tag.key == tag_key
--- lib/inspec/aws/resources/taggable_rds_base.rb
+++ lib/inspec/aws/resources/taggable_rds_base.rb
@@ -3,7 +3,8 @@
 class TaggableRDSBase < RDSBase
   def has_tag?(tag_key = nil, value = nil)
     tags = @resource.tags
-    puts tags.size
+    # c.list_tags_for_resource(resource_name: "arn:aws:rds:us-east-1:086761730243:db:dba-test-postgres").tag_list.map {|x| x['key']}
+    # puts tags.size
     return tags.size > 0 if tag_key.nil?
     tags.each do |tag|
       if tag.key == tag_key
